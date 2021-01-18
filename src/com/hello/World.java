b54e82138cf486e2a04e0f768366f6518830f16f
Ragu <mailmeragu92@gmail.com>
2021-01-18 18:18:10 +0530

    DONE

---
+++ src/com/hello/World.java
@@ -0,0 +1,18 @@
+package com.hello;
+
+public class World {
+
+	private void hello() {
+
+		System.out.println("HELLO WORLD");
+	}
+	
+	public static void main(String[] args) {
+		
+		World w=new World();
+		w.hello();
+		
+
+	}
+
+}
